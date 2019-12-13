4301d62cb83345ef759a5e88ba0774ed49826f30
Guillaume Gay <guillaume@damcb.com>
2019-12-13 17:06:31 +0100

    fin de formation

--- reporter/generator.py
+++ reporter/generator.py
@@ -1,4 +1,36 @@
-def greet():
-    """ A realy minimal function
-    """
-    print("Hello World!")
+import json
+import subprocess
+
+from .header import create_header
+from .graph import make_graph
+
+
+class Generator:
+
+    def __init__(self, json_path, link):
+        self.json_path = json_path
+        self.link = link
+        self.authors = self._get_authors()
+
+    def _get_authors(self):
+        with open(self.json_path, 'r') as jf:
+            authors = json.load(jf)
+        return authors
+
+    def get_header(self):
+        return create_header(self.authors)
+
+    def get_graph(self):
+        self.fig, self.ax = make_graph(self.link)
+
+    def make_report(self):
+        text = self.get_header()
+        self.get_graph()
+        self.fig.savefig('graph.png')
+        text += '\n![el graphe](graph.png)\n'
+        with open("report.md", 'w') as rf:
+            rf.write(text)
+
+    def export(self, fmt):
+
+        return subprocess.run(['pandoc', 'report.md','-o', f'report.{fmt}'])
